57ae227594aa2901e3fc45217e767e9abfaa3037
omvori <cersosimoandrea386@gmail.com>
2025-05-15 17:32:40 +0200

    Add files via upload

---
+++ SQL/esercizi_test_finale.sql
@@ -0,0 +1,40 @@
+select customer_id
+	from customer
+		where active = 1
+		and address_id between 100 and 200
+
+
+--10
+
+select address_id
+	from address
+		where city_id < 600
+		and district in ('Taipei')
+--11
+
+select customer_id
+	from customer
+		where first_name like 'V%'
+--12
+select count (*) as total_customers
+	from customer
+		where first_name like 'V%'
+--13
+select count (rating) 
+	,avg(rental_duration) 
+		from film
+			group by rating
+				order by rating
+
+--14
+select film.film_id
+	from film
+		inner join inventory
+			on inventory.film_id = film.film_id
+		inner join film_category
+			on film_category.film_id = film.film_id
+				where inventory.store_id = 1
+				and film_category.category_id between 4 and 10
+					group by film.film_id
+	
+--15
