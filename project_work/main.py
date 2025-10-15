c3a69879143aa8ceeea7d1c623ec75b7a7ac6cfa
omvori <cersosimoandrea386@gmail.com>
2025-10-15 17:56:24 +0200

    Update main.py

--- project_work/main.py
+++ project_work/main.py
@@ -1,11 +1,8 @@
 from flow import Flow_fly
 
 if __name__ == "__main__":
-    richiesta = input("Inserisci la tua richiesta di prenotazione volo: ")
     
     flow = Flow_fly()
-    flow.state["richiesta"] = richiesta
     
     final = flow.kickoff()
-    print("\n=== RISULTATO FINALE ===")
     print(final)
